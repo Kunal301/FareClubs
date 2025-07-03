0dbd478d43c839d1666ea22bc5657d026808d310
Kunal Soni <kunal@Kunals-MacBook-Air.local>
2025-07-03 10:03:06 +0530

    last commit

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import airportRoutes from "./routes/airportRoutes.js"
 const app = express()
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from your React app
+    origin: "https://fareclubs-flightengine.onrender.com", // Allow requests from your React app
     credentials: true,
   }),
 )
